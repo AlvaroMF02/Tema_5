d52fe501cf23cc96556b68f967bb77cfc6e94d6a
alvaro <alvaromarflo@gmail.com>
2022-01-26 21:22:59 +0100

    ejer 3

---
+++ src/main/java/deberes1/Ejer3.java
@@ -0,0 +1,18 @@
+package deberes1;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class Ejer3 {
+    public static void main(String[] args) {
+        double[] arr = new double[20]; 
+        
+        for (int i = 0; i < arr.length; i++) {  //rellena el array con 7,5
+            arr[i]=7.5;
+        }
+        
+        
+        
+    }
+}
