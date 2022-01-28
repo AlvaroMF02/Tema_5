94091b3eaa67111905bf169de85ce009a204b36d
alvaro <alvaromarflo@gmail.com>
2022-01-28 14:24:13 +0100

    joker qliao

---
+++ src/main/java/jokerbuenomalo/Joker.java
@@ -0,0 +1,39 @@
+package jokerbuenomalo;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alvaro
+ */
+public class Joker {
+    public static void main(String[] args) {
+        //----------------------------------
+        
+        Random numero = new Random();
+        Scanner teclado = new Scanner(System.in);
+        
+        int x = 0;    //guarda los numeros random
+        int bolUse;
+        
+        int [] boleto = new int [7];
+        
+        for (int i = 0; i < boleto.length; i++) {
+            x=numero.nextInt(9 -1 +1) +1;
+            boleto[i]=x;
+        }
+        
+        for (int j : boleto) {      //boleto final 
+            System.out.println(j);
+        }
+        
+        //Pregunta al jambo
+        System.out.println("¿Cuál es su número de boleto?");
+        bolUse=teclado.nextInt();
+        
+        if()
+        
+        
+        
+    } 
+}
