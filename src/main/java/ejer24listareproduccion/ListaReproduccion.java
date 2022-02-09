3c219464812033339172c8f743b92762ea563e4c
alvaro <alvaromarflo@gmail.com>
2022-02-09 22:48:12 +0100

    ññ

--- src/main/java/ejer24listareproduccion/ListaReproduccion.java
+++ src/main/java/ejer24listareproduccion/ListaReproduccion.java
@@ -17,10 +17,29 @@ public class ListaReproduccion {
     
     
     //METODOS
-    public int getNumeroCanciones(){
-        
+    public int getNumeroCanciones(){ //muestra la cantidad de canciones 
         return lista.size();
     }
+    
+    public boolean estaVacia(boolean estado){
+        
+        if (lista.isEmpty()){        //comprueba si la lista esta vacia o llena
+            return estado = true;    //si esta vacia estado sera true
+        }else{
+            return false;
+        }
+    }
+    
+    public int escucharCancion(int posicion){
+        
+      Cancion cancion= lista.get(cancion.toString());   //no se que es esto
+        
+    }
+    
+    
+    
+    
+    
     
     
 }
